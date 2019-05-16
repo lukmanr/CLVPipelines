d733da5e7a8aec3b21f0df11cdb07956325f0c80
jarokaz <jarokaz>
2019-05-16 17:18:31 +0000

    Updates

--- pipelines/clv_train_bigquery.py
+++ pipelines/clv_train_bigquery.py
@@ -5,7 +5,7 @@ import kfp.dsl as dsl
 import kfp.gcp as gcp
 
 BASE_IMAGE = 'gcr.io/sandbox-235500/automltablesbase:dev'
-QUERY_TEMPLATE_URI = 'gs://sandbox-235500/sql-templates/create_features.sql'
+QUERY_TEMPLATE_URI = 'gs://sandbox-235500/sql-templates/create_features_template.sql'
 BIGQUERY_COMPONENT_SPEC_URI = 'https://raw.githubusercontent.com/kubeflow/pipelines/3b938d664de35db9401c6d198439394a9fca95fa/components/gcp/bigquery/query/component.yaml'
 AML_IMPORT_DATASET_SPEC_URI = '/home/jupyter/projects/clv_kfp/components/automl_tables/aml-import-dataset.yaml'
 AML_TRAIN_MODEL_SPEC_URI = '/home/jupyter/projects/clv_kfp/components/automl_tables/aml-train-model.yaml'
@@ -57,7 +57,7 @@ def clv_pipeline(
     automl_compute_region='us-central1',
     automl_dataset_name='clv_features',
     model_name='clv_regression',
-    train_budget='3000',
+    train_budget='1000',
     #target_name='target_monetary',
     #features_to_exclude='monetary'
 ):
