12f7fc25478c00ed26fba152fc272872759b3749
jarokaz <jarokaz>
2019-05-21 03:32:35 +0000

    updates

--- components/automl_tables/build_image.sh
+++ components/automl_tables/build_image.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-image_name=gcr.io/sandbox-235500/automl-tables-components
+image_name=gcr.io/clv-pipelines/automl-tables-components
 image_tag=latest
 full_image_name=${image_name}:${image_tag}
 
