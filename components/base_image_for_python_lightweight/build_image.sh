0785b4111f4067a99ca951e017a501f2d0ee2551
Jarek Kazmierczak <jarokaz@hotmail.com>
2019-05-21 13:13:13 -0700

    Update build_image.sh

--- components/base_image_for_python_lightweight/build_image.sh
+++ components/base_image_for_python_lightweight/build_image.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-image_name=gcr.io/clv-pipelines/base-image
+if [ "$1" == ""]; then
+    repo = "clv-pipelines"
+else
+    repo = "$1"
+
+image_name=gcr.io/$repo/base-image
 image_tag=latest
 full_image_name=${image_name}:${image_tag}
 
-docker build -t "${full_image_name}" .
-docker push "${full_image_name}" 
+echo $full_image_name
+
+#docker build -t "${full_image_name}" .
+#docker push "${full_image_name}" 
