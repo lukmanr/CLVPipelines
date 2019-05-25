815c01f6a11937a6cc738e5da9e91192910ee638
jarokaz <jarokaz>
2019-05-25 21:28:43 +0000

    updates

--- components/automl_tables/src/batch_predict.py
+++ components/automl_tables/src/batch_predict.py
@@ -16,6 +16,7 @@
 
 import argparse
 import logging
+import time
 from pathlib import Path
 from google.cloud import automl_v1beta1 as automl
 
@@ -30,24 +31,28 @@ def batch_predict(client, project_id, region, model_id, datasource, output):
   if datasource.startswith("bq"):
     input_config = {"bigquery_source": {"input_uri": datasource}}
   else:
-    input_uris = datasource.split(",").strip()
+    input_uris = datasource.split(",")
     input_config = {"gcs_source": {"input_uris": input_uris}}
 
   if output.startswith("bq"):
-  #  output_config = {"bigquery_destination": {"output_uri": output}}
     output_config = {"bigquery_destination": {"output_uri": output}}
   else:
-    output_uris = output.split(",").strip()
-    output_config = {"gcs_destination": {"output_uris": output_uris}}
+    output_config = {"gcs_destination": {"output_uri_prefix": output}}
 
   # Run the prediction query
   response = client.batch_predict(
       model_full_id, input_config, output_config)
+    
 
   # Wait for completion
+
+  # while response.done() is False:
+  #  time.sleep(1)
+  # result = response.result()
+
   response.result()
 
-  return  response.metadata
+  return "result"
 
 
 def _parse_arguments():
@@ -68,6 +73,11 @@ def _parse_arguments():
       type=str,
       required=True,
       help="A region for the AutoML Tables inference")
+  parser.add_argument(
+      "--model-id",
+      type=str,
+      required=True,
+      help="An Automl model")
   parser.add_argument(
       "--datasource",
       type=str,
@@ -96,11 +106,12 @@ if __name__ == "__main__":
       client,
       args.project_id,
       args.region,
-      args.region,
+      args.model_id,
       args.datasource,
       args.output)
-  logging.info("Batch scoring completed: {}:".format(result))
+  logging.info("Batch scoring completed")
 
   # Save results
+  # TBD ******
   Path(args.output).parent.mkdir(parents=True, exist_ok=True)
   Path(args.output).write_text(result)
--- components/automl_tables/tests/run_predict.sh
+++ components/automl_tables/tests/run_predict.sh
@@ -3,6 +3,15 @@
 python3 ../src/batch_predict.py \
 --project-id=sandbox-235500 \
 --region=us-central1 \
+--model-id=TBL403503175207747584 \
 --datasource=bq://sandbox-235500.clv_dataset.test_features \
 --output=bq://sandbox-235500 
+#--key_file=key.json \
+
+#python3 ../src/batch_predict.py \
+#--project-id=sandbox-235500 \
+#--region=us-central1 \
+#--model-id=TBL403503175207747584 \
+#--datasource=gs://clv-testing/test-features/features.csv \
+#--output=gs://clv-testing/predictions 
 #--key_file=key.json \
