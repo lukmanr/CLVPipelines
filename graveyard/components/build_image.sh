f41212075bf622a80d32c3c57f6274e19c8423d5
jarokaz <jarokaz>
2019-06-06 03:53:22 +0000

    Initial draft

--- graveyard/components/build_image.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-image_name=gcr.io/sandbox-235500/clv-components
-image_tag=latest
-full_image_name=${image_name}:${image_tag}
-
-docker build -t "${full_image_name}" .
-docker push "${full_image_name}" 
