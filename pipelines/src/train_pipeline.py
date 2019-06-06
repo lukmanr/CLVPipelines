340a2c7b2e6f63b7a31991a353292f8c0bd9844f
jarokaz <jarokaz>
2019-06-06 01:35:49 +0000

    Updated training pipeline

--- pipelines/src/train_pipeline.py
+++ pipelines/src/train_pipeline.py
@@ -20,7 +20,7 @@ from kfp import gcp
 
 # Initialize component store
 component_store = kfp.components.ComponentStore()
-platform = "GCP"
+use_sa_secret = False
 
 # Pipeline definition
 @kfp.dsl.pipeline(
@@ -43,11 +43,11 @@ def clv_train(
     aml_compute_region='us-central1',
     train_budget='1000',
     target_column_name='target_monetary',
-    features_to_exclude='customer_id',
+    features_to_exclude='["customer_id"]',
     optimization_objective='MINIMIZE_MAE',
     primary_metric='mean_absolute_error',
     deployment_threshold=900, 
-    skip_deployment=True,
+    skip_deployment=False,
     query_template_uri='gs://clv-pipelines/scripts/create_features_template.sql'
 ):
     # Create component factories
@@ -59,7 +59,7 @@ def clv_train(
     import_dataset_op = component_store.load_component('aml-import-dataset') 
     train_model_op = component_store.load_component('aml-train-model')  
     deploy_model_op = component_store.load_component('aml-deploy-model')  
-    retrieve_metrics_op = component_store.load_component('aml-retrieve-metrics')
+    log_metrics_op = component_store.load_component('aml-log-metrics')
 
     # Define workflow
 
@@ -104,7 +104,7 @@ def clv_train(
     # Import BQ table with features into AML dataset
     import_dataset = import_dataset_op(
         project_id=project_id,
-        location=aml_compute_region,
+        region=aml_compute_region,
         dataset_name=aml_dataset_name,
         description='',
         source_data_uri=source_data_uri,
@@ -117,7 +117,7 @@ def clv_train(
     # Train the model
     train_model = train_model_op(
         project_id=project_id,
-        location=aml_compute_region,
+        region=aml_compute_region,
         dataset_id=import_dataset.outputs['output_dataset_id'],
         model_name=aml_model_name,
         train_budget=train_budget,
@@ -127,23 +127,28 @@ def clv_train(
         features_to_exclude=features_to_exclude
         )
 
-    """
-    # If MAE is above the threshold deploy the model
-    with kfp.dsl.Condition(retrieve_metrics.outputs['output_mae'] < deployment_threshold):
-        deploy_model = deploy_model_op(train_model.output)
+    # Log evaluation metrics
+    log_metrics = log_metrics_op(
+        model_full_id = train_model.outputs['output_model_full_id']
+    )
 
-   # Configure the pipeline to use GCP service account secret if running on GCP
-    steps = [load_sales_transactions,
-             prepare_feature_engineering_query,
-             engineer_features,
-             import_dataset,
-             train_model,
-             deploy_model]
-    for step in steps:
-        if platform == 'GCP':
-            step.apply(gcp.use_gcp_secret('user-gcp-sa'))
+    # Deploy the model if configured and the primary metric below the threshold
+    with kfp.dsl.Condition(skip_deployment != True):
+        with kfp.dsl.Condition(train_model.outputs['output_primary_metric_value'] < deployment_threshold):
+            deploy_model = deploy_model_op(train_model.outputs['output_model_full_id'])
 
-    """
+    # Configure the pipeline to use a service account secret 
+    if use_sa_secret: 
+        steps = [load_sales_transactions,
+                prepare_feature_engineering_query,
+                engineer_features,
+                import_dataset,
+                train_model,
+                log_metrics,
+                deploy_model]
+        for step in steps:
+                step.apply(gcp.use_gcp_secret('user-gcp-sa'))
+
 
 def _compile_pipeline(output_dir, local_search_paths, url_search_prefixes, platform='GCP', type_check=False):
     """Compile the pipeline"""
