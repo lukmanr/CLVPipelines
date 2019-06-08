6157ff9783a83cee6ad12e7e3c264873302ad4d5
Jarek Kazmierczak <jarekk@google.com>
2019-06-08 17:23:43 +0000

    Updated host

--- pipelines/run/run_train.sh
+++ pipelines/run/run_train.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 python3 run_pipeline.py \
---host https://jkkf.endpoints.sandbox-235500.cloud.goog/_/pipeline-dashboard \
+--host https://jkkf.endpoints.sandbox-235500.cloud.goog/pipeline \
 --client_id 165540728514-vdmfatc4jdch6bkuvsfcd0opfd5bipg6.apps.googleusercontent.com \
 --experiment CLV_TRAIN \
 --run-name TESTING_RUN \
@@ -16,6 +16,6 @@ python3 run_pipeline.py \
 "predict_end": "2011-12-12", \
 "threshold_date": "2011-08-08", \
 "max_monetary": 15000, \
-"query_template_uri": "gs://clv-testing/scripts/create_features_template.sql" \
+"query_template_uri": "gs://clv-testing/scripts/create_features_template.sql", \
 "skip_deployment": True \
 }'
