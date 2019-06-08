734000ba9be097861e1b6ab85d2254e7fc23b69f
Jarek Kazmierczak <jarekk@google.com>
2019-06-08 23:10:30 +0000

    Testing cloud build

--- pipelines/compile_pipelines.sh
+++ pipelines/compile_pipelines.sh
@@ -3,9 +3,3 @@
 dsl-compile --py src/train_pipeline.py --output clv_train.tar.gz --disable-type-check
 dsl-compile --py src/batch_predict_pipeline.py --output clv_batch_predict.tar.gz --disable-type-check
 
-
-var=$(ls -la)
-echo "$var"
-
-var=$(ls -la ../)
-echo "$var"
