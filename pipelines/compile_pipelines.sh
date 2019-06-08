83df133f20759290e78b0cf64e5c0c4b9210d8b3
Jarek Kazmierczak <jarekk@google.com>
2019-06-08 21:53:18 +0000

    testing Cloud Build

--- pipelines/compile_pipelines.sh
+++ pipelines/compile_pipelines.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-dsl-compile --py src/train_pipeline.py --output compiled/clv_train.tar.gz --disable-type-check
-dsl-compile --py src/batch_predict_pipeline.py --output compiled/clv_batch_predict.tar.gz --disable-type-check
+echo "In pipelines"
+
+#dsl-compile --py src/train_pipeline.py --output compiled/clv_train.tar.gz --disable-type-check
+#dsl-compile --py src/batch_predict_pipeline.py --output compiled/clv_batch_predict.tar.gz --disable-type-check
