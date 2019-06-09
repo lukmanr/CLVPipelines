2be482ed1677e413511954084056852565da7d30
Jarek Kazmierczak <jarekk@google.com>
2019-06-09 21:29:53 +0000

    Testing cloud build

--- pipelines/configure_compiler_directives.py
+++ pipelines/configure_compiler_directives.py
@@ -19,11 +19,20 @@ from pathlib import Path
 from jinja2 import Template
 
 
-def configure_compiler_directives(config_template, config_py, directives):
+def configure_compiler_directives(
+  config_template='src/config.py.jinja', 
+  config_py='src/config.py', 
+  local_search_paths=["../components/automl_tables/specs"],
+  url_search_prefixes=["https://raw.githubusercontent.com/kubeflow/pipelines/3b938d664de35db9401c6d198439394a9fca95fa/components/gcp/"],
+  use_sa_secret=True):
   """Generates config.py that configures KFP compilation behaviour"""
 
   template = Template(Path(config_template).read_text())
-  compiler_directives = template.render(directives)
+  compiler_directives = template.render(
+    local_search_paths=local_search_paths,
+    url_search_prefixes=url_search_prefixes,
+    use_sa_secret=use_sa_secret)
+
   Path(config_py).parent.mkdir(parents=True, exist_ok=True)
   Path(config_py).write_text(compiler_directives)
 
