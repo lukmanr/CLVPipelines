67be7161268bfa0a2af8a855f429385fb224b9bd
Jarek Kazmierczak <jarokaz@hotmail.com>
2019-06-18 11:19:37 -0700

    Update run_batch_predict.sh

--- run/run_batch_predict.sh
+++ run/run_batch_predict.sh
@@ -2,8 +2,8 @@
 
 python run_pipeline.py \
 --host https://jkkf.endpoints.sandbox-235500.cloud.goog/pipeline \
---experiment CLV_BATCH_PREDICT \
---run-name TESTING_RUN \
+--experiment "CLV batch predit" \
+--run-name "Batch predict run" \
 --pipeline_file clv_predict.tar.gz \
 --arguments '{\
 "project_id": "clv-prod", \
