ee71ce9a1392889c055dec729f888da6e3969c1a
Jarek Kazmierczak <jarokaz@hotmail.com>
2019-06-18 11:23:57 -0700

    Update run_batch_predict.sh

--- run/run_batch_predict.sh
+++ run/run_batch_predict.sh
@@ -11,6 +11,6 @@ python run_pipeline.py \
 "predict_end": "2011-12-12", \
 "threshold_date": "2011-08-08", \
 "max_monetary": 15000, \
-"aml_model_id": "TBL8120491505451270144", \
+"aml_model_id": "TBL8408686697230893056", \
 "destination_prefix": "bq://clv-dev", \
 }'
