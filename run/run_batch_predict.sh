982c82a29f957ea592bc714598f733880ab650e7
Jarek Kazmierczak <jarokaz@hotmail.com>
2019-06-18 11:34:32 -0700

    Update run_batch_predict.sh

--- run/run_batch_predict.sh
+++ run/run_batch_predict.sh
@@ -8,6 +8,8 @@ python run_pipeline.py \
 --arguments '{\
 "project_id": "clv-prod", \
 "source_gcs_path": "gs://clv-accelerator/sample-dataset/transactions.csv", \
+"source_bq_table": "", \
+"bq_dataset_name": "", \
 "predict_end": "2011-12-12", \
 "threshold_date": "2011-08-08", \
 "max_monetary": 15000, \
