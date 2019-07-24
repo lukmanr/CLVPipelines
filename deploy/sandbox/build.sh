3a5e90cdeb63bfcd367d9faba345b6404995a469
jarekk <jarokaz@hotmail.com>
2019-07-24 14:21:11 -0700

    Updates to kfp-helper Dockerfile

--- deploy/sandbox/build.sh
+++ deploy/sandbox/build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 SUBSTITUTIONS=\
-_CLV_REPO=jarokaz/CLVPipelines
+_CLV_REPO=jarokaz/CLVPipelines,\
+_CLUSTER=jkk8s-3,\
+_ZONE=us-central1-a
 
 gcloud builds submit --no-source --config cloudbuild.yaml \
 --substitutions $SUBSTITUTIONS
