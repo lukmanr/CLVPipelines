3e39230ce61bd95a801b44082ffbceb0eb8fa985
Jarek Kazmierczak <jarokaz@hotmail.com>
2019-07-29 11:57:59 -0700

    Update run_batch_predict.sh

--- run/run_batch_predict.sh
+++ run/run_batch_predict.sh
@@ -6,13 +6,13 @@ run_pipeline \
 --run-name "Batch predict run" \
 --pipeline_name predict_pipeline \
 --params '{\
-"project_id": "jktest6", \
-"source_gcs_path": "gs://jkclv-bucket2/dataset/test_transactions.csv", \
+"project_id": "kfp-clv", \
+"source_gcs_path": "gs://kfp-clv-artifacts/dataset/test_transactions.csv", \
 "source_bq_table": "", \
 "bq_dataset_name": "", \
 "predict_end": "2011-12-12", \
 "threshold_date": "2011-08-08", \
 "max_monetary": 15000, \
 "aml_model_id": "TBL9153944474630488064", \
-"destination_prefix": "bq://jktest6", \
+"destination_prefix": "bq://kfp-clv", \
 }'
